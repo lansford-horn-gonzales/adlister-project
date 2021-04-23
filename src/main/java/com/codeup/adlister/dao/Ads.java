ccc73ce76aba06c81c1b449c4293066c9c84c8c5
emihhorn <emilyhopehorn@gmail.com>
2021-04-23 11:04:08 -0500

    functionality complete

--- src/main/java/com/codeup/adlister/dao/Ads.java
+++ src/main/java/com/codeup/adlister/dao/Ads.java
@@ -10,6 +10,7 @@ public interface Ads {
     List<Ad> all();
     // insert a new ad and return the new ad's id
     Long insert(Ad ad);
-
+    void delete(long adId);
     List<Ad> searchAdsFromResults(String searchAds) throws SQLException;
+
 }
