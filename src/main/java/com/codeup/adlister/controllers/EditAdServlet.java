162ad31183785d72703fa7d53b9480391017b931
emihhorn <emilyhopehorn@gmail.com>
2021-04-27 09:04:57 -0500

    css finished

--- src/main/java/com/codeup/adlister/controllers/EditAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/EditAdServlet.java
@@ -16,37 +16,39 @@ public class EditAdServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+//Setting the value of adId into parameter of edit id.
         long adId = Long.parseLong(request.getParameter("editAd"));
-        try {
+        try { //setting the attribute of ad using the Dao
             request.setAttribute("ad", DaoFactory.getAdsDao().findAdById(adId));
         } catch (SQLException e) {
             e.printStackTrace();
         }
         request.getRequestDispatcher("/WEB-INF/edit-ads.jsp").forward(request, response);
     }
-
+// Setting values of image, title , description parameters
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         String image = request.getParameter("image");
         String title = request.getParameter("title");
         String description = request.getParameter("description");
-//        Ad oldAd = (Ad) request.getSession().getAttribute("ad");
+// Getting the parameter of id.
         long id = Long.parseLong(request.getParameter("id"));
+//Getting the old ad by its id.
         Ad oldAd = null;
         try {
             oldAd = DaoFactory.getAdsDao().findAdById(id);
         } catch (SQLException exception) {
             exception.printStackTrace();
         }
-
+//editing the old ad with a new ad by using the old ad id.
         Ad editAd = new Ad(oldAd.getId(), image, title, description);
         try {
             DaoFactory.getAdsDao().editAd(oldAd, editAd);
         } catch (SQLException e) {
             e.printStackTrace();
         }
+//Getting the latest update from the database to display on the page.
         request.getSession().setAttribute("ad",editAd);
         response.sendRedirect("/ads");
     }
--- src/main/webapp/WEB-INF/ads/ad-page.jsp
+++ src/main/webapp/WEB-INF/ads/ad-page.jsp
@@ -5,7 +5,8 @@
     <jsp:include page="/WEB-INF/partials/head.jsp">
         <jsp:param name="title" value="Welcome to my site!" />
     </jsp:include>
-    <link rel="stylesheet" type="text/css" href="css/main.css">
+<%--    <link rel="stylesheet" type="text/css" href="css/main.css">--%>
+    <link rel="stylesheet" type="text/css" href="css/card.css">
 </head>
 <body>
 <jsp:include page="/WEB-INF/partials/login-navbar.jsp"/>
